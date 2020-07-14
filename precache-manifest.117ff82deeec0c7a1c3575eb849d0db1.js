8deab6e20c6dab270a7aabe37004faf9317d3bea
yamini465 <40388091+yamini465@users.noreply.github.com>
2020-07-14 14:24:36 +0530

    Updates

---
+++ precache-manifest.117ff82deeec0c7a1c3575eb849d0db1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "959efbc7c8029e6287739a31edabd058",
+    "url": "/chatbot/index.html"
+  },
+  {
+    "revision": "956c652c4368a5326445",
+    "url": "/chatbot/static/css/main.7bdd169a.chunk.css"
+  },
+  {
+    "revision": "882d6abc4afdfecc5154",
+    "url": "/chatbot/static/js/2.ab5a825a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chatbot/static/js/2.ab5a825a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "956c652c4368a5326445",
+    "url": "/chatbot/static/js/main.31d2b2eb.chunk.js"
+  },
+  {
+    "revision": "6da786bfa225ae5459b9",
+    "url": "/chatbot/static/js/runtime-main.253fb56a.js"
+  }
+]);
